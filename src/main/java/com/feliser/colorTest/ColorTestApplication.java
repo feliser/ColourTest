2051a4357f679faad3a5999a0dc8e9c0bea17f00
feliser <bonopoape@gmail.com>
2020-01-05 01:27:56 -0500

    Initial Upload

---
+++ src/main/java/com/feliser/colorTest/ColorTestApplication.java
@@ -0,0 +1,13 @@
+package com.feliser.colorTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feliser/colorTest/ColorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feliser.colorTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
