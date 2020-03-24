1b61e19bd7486488b4d375e799659e05a05ecbe7
feliser <bonopoape@gmail.com>
2020-03-24 16:14:51 -0400

    Add ColorTestDatabseService.java

---
+++ src/main/java/com/feliser/colorTest/services/ColorTestDatabaseService.java
@@ -0,0 +1,12 @@
+package com.feliser.colorTest.services;
+
+import org.springframework.stereotype.Service;
+
+import com.feliser.colorTest.models.ColorTestDatabaseRequestModel;
+
+@Service
+public class ColorTestDatabaseService {
+	public void uploadScore(ColorTestDatabaseRequestModel request) {
+		
+	}
+}
