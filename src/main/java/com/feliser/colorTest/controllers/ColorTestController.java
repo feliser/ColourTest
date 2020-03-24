66c8eac8660c2ea293b07a3cf0628bd26be89181
feliser <bonopoape@gmail.com>
2020-03-24 16:16:01 -0400

    Add ColorTestController.java

---
+++ src/main/java/com/feliser/colorTest/controllers/ColorTestController.java
@@ -0,0 +1,18 @@
+package com.feliser.colorTest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ColorTestController {
+	@GetMapping("/test")
+	public String getTest(Model model,
+			@RequestParam(value = "leftColor", required = false, defaultValue = "Green") String leftColor,
+			@RequestParam(value = "rightColor", required = false, defaultValue = "Yellow") String rightColor) {
+		model.addAttribute("leftColor", leftColor);
+		model.addAttribute("rightColor", rightColor);
+		return "test";
+	}
+}
