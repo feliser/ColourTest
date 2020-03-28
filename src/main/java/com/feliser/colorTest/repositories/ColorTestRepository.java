7b96db7711738195877b8c449e3fadb1d805d703
feliser <bonopoape@gmail.com>
2020-03-27 22:02:03 -0400

    Remove ColorTestRepository.java

--- src/main/java/com/feliser/colorTest/repositories/ColorTestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.feliser.colorTest.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.feliser.colorTest.models.ColorTestDatabaseModel;
-
-public interface ColorTestRepository extends CrudRepository<ColorTestDatabaseModel, Integer> {}
